9cd4893fec2f9afb81a90d1519ba77569b917e3f
Paul Min <isimplyexcel@gmail.com>
2019-10-31 17:18:27 -0700

    login and logout implemented

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Paul Min on 10/31/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+    }
+}
